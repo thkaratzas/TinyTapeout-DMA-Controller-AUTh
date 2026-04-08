148815fffd4a2471bfab97ccdbf150599825cd73
Athanasios Karatzas <168374809+thkaratzas@users.noreply.github.com>
2026-04-09 00:44:46 +0300

    Refactor testbench for DMA module instantiation

--- test/tb.v
+++ test/tb.v
@@ -1,17 +1,15 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
 module tb ();
 
-  // VCD dump
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
-
-  // Inputs and outputs
-  reg clk;
+  // Wire up the inputs and outputs:
+  reg clk;        // DMAC/CPU clock
+  reg mem_clk;    // Memory clock
+  reg io_clk;     // I/O Device clock
   reg rst_n;
   reg ena;
   reg [7:0] ui_in;
@@ -19,26 +17,36 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-
+  wire _keep_clocks = clk ^ mem_clk ^ io_clk; // so the simulator doesn't optimize away the unused clocks
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
-  // DUT instantiation
-  tt_um_dma user_project1 (
+  initial begin
+    clk = 1'b0;
+    mem_clk = 1'b0;
+    io_clk = 1'b0;
+  end
+
+  // Replace tt_um_example with your module name:
+  tt_um_auth_dmac dut (
+
+      // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
+
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
   );
+
 
 endmodule
